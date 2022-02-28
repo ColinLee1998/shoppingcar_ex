422c713cf428a69cb739ee16164a1aa15b15553d
ColinLee1998 <colinlee12171998@gmail.com>
2022-03-01 02:33:11 +0800

    20220301 設定登入機制

---
+++ session/login_input.php
@@ -0,0 +1,9 @@
+<form action="login_output.php" method="post">
+    <div>
+        帳號 :<input type="text" name="login"><br>
+    </div>
+    <div>
+        密碼 :<input type="text" name="password"><br>
+    </div>
+    <input type="submit" value="登入">
+</form>
